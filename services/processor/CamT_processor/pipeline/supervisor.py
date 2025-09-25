520b81d235aab6893a6cf5f72826500eda5e1b9b
mcassar4 <atvman8@gmail.com>
2025-09-25 18:01:48 -0400

    Add supervisor start bootstrap - Set multiprocessing start method to spawn - Configure logging from LOG_LEVEL env var - Parse camera sources with poll interval

--- services/processor/CamT_processor/pipeline/supervisor.py
+++ services/processor/CamT_processor/pipeline/supervisor.py
@@ -1,7 +1,11 @@
 import logging
+import os
 from multiprocessing import Event, Queue, set_start_method
 
 from ..config.settings import ProcessorSettings
+from ..logging_utils import configure_logging
+from .ingestion import IngestionWorker, parse_camera_sources
+
 
 logger = logging.getLogger("processor.supervisor")
 
@@ -14,3 +18,13 @@ class Supervisor:
         self.frame_queue = Queue(maxsize=settings.queue_size)
         self.person_queue = Queue(maxsize=settings.queue_size)
         self.vehicle_queue = Queue(maxsize=settings.queue_size)
+
+    def start(self) -> None:
+        try:
+            set_start_method("spawn")
+        except RuntimeError:
+            pass
+        configure_logging(os.getenv("LOG_LEVEL"))
+        cameras = parse_camera_sources(self.settings.camera_sources, self.settings.frame_poll_interval)
+        if not cameras:
+            logger.warning("No camera sources configured; processor will idle until sources are provided")
