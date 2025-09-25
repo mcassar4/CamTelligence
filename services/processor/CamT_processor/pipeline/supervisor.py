dc2e845cb3cb2b9cc93cdd9c90a6dc19f5fe148f
mcassar4 <atvman8@gmail.com>
2025-09-25 19:56:48 -0400

    Add worker factories and process startup - Define ingestion and detection worker factories - Instantiate workers into processes registry - Start all worker processes on supervisor start

--- services/processor/CamT_processor/pipeline/supervisor.py
+++ services/processor/CamT_processor/pipeline/supervisor.py
@@ -5,7 +5,7 @@ from multiprocessing import Event, Queue, set_start_method
 from ..config.settings import ProcessorSettings
 from ..logging_utils import configure_logging
 from .ingestion import IngestionWorker, parse_camera_sources
-
+from .detection import DetectionWorker
 
 logger = logging.getLogger("processor.supervisor")
 
@@ -28,3 +28,22 @@ class Supervisor:
         cameras = parse_camera_sources(self.settings.camera_sources, self.settings.frame_poll_interval)
         if not cameras:
             logger.warning("No camera sources configured; processor will idle until sources are provided")
+        
+        factories = {
+            "ingestion": lambda: IngestionWorker(self.frame_queue, cameras=cameras, stop_event=self.stop_event),
+            "detection": lambda: DetectionWorker(
+                self.frame_queue,
+                self.person_queue,
+                self.vehicle_queue,
+                self.stop_event,
+                motion_history=self.settings.motion_history,
+                motion_kernel_size=self.settings.motion_kernel_size,
+                motion_min_area=self.settings.motion_min_area,
+                motion_debug_dir=self.settings.motion_debug_dir,
+                motion_max_foreground_ratio=self.settings.motion_max_foreground_ratio,
+            )
+        }
+
+        self.processes = {name: factory() for name, factory in factories.items()}
+        for proc in self.processes.values():
+            proc.start()
