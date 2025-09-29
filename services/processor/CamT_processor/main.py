b0dd19d4d696b2f2fbb4f9f185575f88dc25bd9e
mcassar4 <atvman8@gmail.com>
2025-09-28 22:51:50 -0400

    Refactor main runner adding Settings and supervisor

--- services/processor/CamT_processor/main.py
+++ services/processor/CamT_processor/main.py
@@ -1,10 +1,16 @@
 import logging
 
+from .config.settings import ProcessorSettings
 from .logging_utils import configure_logging
+from .pipeline.supervisor import Supervisor
 
 
 def main() -> None:
     configure_logging()
+    settings = ProcessorSettings()
+
+    supervisor = Supervisor(settings)
+    supervisor.start()
 
 
 if __name__ == "__main__":
